08d46cc39ef030826111410c7de5a50edd74cf8a
“dammyson” <voecmanager@yahoo.com>
2021-04-12 21:39:54 +0100

    new changes

--- app/src/main/java/com/raywenderlich/android/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/simplecalculator/MainActivity.kt
@@ -50,7 +50,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     CalculatorEngine()
   }
 
-  private val operands = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".")
+  private val operands = listOf("0", "1", "3", "3", "4", "5", "6", "7", "8", "9", ".")
 
   override fun onCreate(savedInstanceState: Bundle?) {
     setTheme(R.style.AppTheme)
